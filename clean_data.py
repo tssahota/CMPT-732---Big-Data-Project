d2b32ddcd4fea192cbe8f693f5300a4875067061
hwec0112 <hwec0112@gmail.com>
2020-12-03 01:44:14 -0800

    complete clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,75 +1,19 @@
 import sys
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
 from pyspark.sql import SparkSession, functions, types, Row
-#clean the movies data
 
-# movie_schema = types.StructType(
-#     [
-#         types.StructField("adult", types.StringType()),
-#         types.StructField("belongs_to_collection", types.StringType()),
-#         types.StructField("budget", types.StringType()),
-#         types.StructField("genres", types.StringType()),     
-#         types.StructField("homepage", types.StringType()),
-#         types.StructField("id", types.StringType()),
-#         types.StructField("imdb_id", types.StringType()),
-#         types.StructField("original_language", types.StringType()),
-#         types.StructField("original_title", types.StringType()),
-#         types.StructField("overview", types.StringType()),
-#         types.StructField("popularity", types.StringType()),
-#         types.StructField("poster_path", types.StringType()),
-#         types.StructField("production_companies", types.StringType()),
-#         types.StructField("production_countries", types.StringType()),
-#         types.StructField("release_date", types.StringType()),
-#         types.StructField("revenue", types.StringType()),
-#         types.StructField("runtime", types.StringType()),
-#         types.StructField("spoken_languages", types.StringType()),
-#         types.StructField("status", types.StringType()),
-#         types.StructField("tagline", types.StringType()),
-#         types.StructField("title", types.StringType()),
-#         types.StructField("video", types.StringType()),
-#         types.StructField("vote_average", types.StringType()),
-#         types.StructField("vote_count", types.StringType()),
-#     ]
-# )
-
-'''movie_schema = types.StructType(
-    [
-        types.StructField("adult", types.BooleanType()),
-        #types.StructField("belongs_to_collection", types.MapType(types.IntegerType(), types.StringType(), types.StringType(), types.StringType(), types.StringType())),
-        types.StructField("belongs_to_collection", types.StringType()),
-        types.StructField("budget", types.IntegerType()),
-        #types.StructField("genres", types.ArrayType(types.MapType(types.IntegerType(), types.StringType()))),
-        types.StructField("genres", types.StringType()),
-        types.StructField("homepage", types.StringType()),
-        types.StructField("id", types.IntegerType()),
-        types.StructField("imdb_id", types.StringType()),
-        types.StructField("original_language", types.StringType()),
-        types.StructField("original_title", types.StringType()),
-        types.StructField("overview", types.StringType()),
-        types.StructField("popularity", types.DoubleType()),
-        types.StructField("poster_path", types.StringType()),
-        types.StructField("production_companies", types.StringType()),
-        types.StructField("production_countries", types.StringType()),
-        types.StructField("release_date", types.TimestampType()),
-        types.StructField("revenue", types.IntegerType()),
-        types.StructField("runtime", types.IntegerType()),
-        #types.StructField("spoken_languages", types.ArrayType(types.MapType(types.StringType(), types.StringType()))),
-        types.StructField("spoken_languages", types.StringType()),
-        types.StructField("status", types.StringType()),
-        types.StructField("tagline", types.StringType()),
-        types.StructField("title", types.StringType()),
-        types.StructField("video", types.BooleanType()),
-        types.StructField("vote_average", types.FloatType()),
-        types.StructField("vote_count", types.IntegerType()),
-    ]
-)'''
-
+def calculate_return(revenue, budget):
+    if revenue and budget:
+        return revenue/budget
+    else:
+        return None
 
 def main(inputs, output):
-    movie_data = spark.read.csv(inputs, schema=movie_schema, header=True)
+    movie_data = spark.read.csv(inputs, header=True)
+    #change all revenue and budget == 0 to null to avoid calculation error 
     movie_data = movie_data.select(movie_data['adult'].cast('boolean'),
                                    movie_data['belongs_to_collection'],
-                                   movie_data['budget'].cast('int'),
+                                   movie_data['budget'].cast('int').alias('ori_budget'),
                                    movie_data['genres'],  
                                    movie_data['homepage'],
                                    movie_data['id'].cast('int'),
@@ -81,8 +25,8 @@ def main(inputs, output):
                                    movie_data['poster_path'],
                                    movie_data['production_companies'],
                                    movie_data['production_countries'],
-                                   movie_data['release_date'],
-                                   movie_data['revenue'].cast('long'),
+                                   movie_data['release_date'].cast('timestamp'),
+                                   movie_data['revenue'].cast('long').alias('ori_revenue'),
                                    movie_data['runtime'].cast('int'),
                                    movie_data['spoken_languages'],
                                    movie_data['status'],
@@ -91,9 +35,19 @@ def main(inputs, output):
                                    movie_data['video'].cast('boolean'),
                                    movie_data['vote_average'].cast('float'),
                                    movie_data['vote_count'].cast('int'))
-    movie_data.show(20)
-
-
+    #replace zero by Nones
+    moeny_udf = functions.udf(lambda value: None if value == 0 else value, types.IntegerType())
+    movie_data = movie_data.withColumn('revenue', moeny_udf(movie_data['ori_revenue']))
+    movie_data = movie_data.withColumn('budget', moeny_udf(movie_data['ori_budget']))
+    #remove useless features
+    movie_data = movie_data.drop('imdb_id', 'original_title', 'adult', 'ori_revenue', 'ori_budget')
+    #calculate return as a new column
+    return_udf = functions.udf(calculate_return, types.FloatType())
+    movie_data = movie_data.withColumn('return', return_udf(movie_data['budget'], movie_data['revenue']))
+    #check dataframe
+    #movie_data = movie_data.select(movie_data['revenue'], movie_data['budget'], movie_data['return'])
+    #movie_data.show(100)
+    movie_data.write.parquet(output, mode='overwrite')
 
 if __name__ == '__main__':
     input = sys.argv[1]
