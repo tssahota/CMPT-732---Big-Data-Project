7a8fd1dfcb1b689dc0ebc306fda8a845b13a25ff
hwec0112 <hwec0112@gmail.com>
2020-12-07 04:46:55 -0800

    add visulation for data analysis

--- eric_analysis.py
+++ eric_analysis.py
@@ -9,7 +9,7 @@ def main(movie_path, genre_path):
     #popularity
     movie_data.describe('popularity').show()
     #movie_data.show(10)
-    movie_data = movie_data.select( movie_data["genre_ids"], movie_data["title"], movie_data["popularity"], movie_data["release_date"])
+    movie_data = movie_data.select( movie_data["vote_average"], movie_data["title"], movie_data["popularity"], movie_data["release_date"])
     genre_data.orderBy(genre_data['genre_id'].desc()).show(50)
     #remove null data slot
     movie_data = movie_data.na.drop(subset=["title", "release_date", "popularity", "genre_ids"])
@@ -18,14 +18,15 @@ def main(movie_path, genre_path):
     # print(movie_data.where(movie_data["title"].isNull()).count())
 
     movie_data = movie_data.withColumn('year',year(to_timestamp(movie_data['release_date'], 'yyyy-MM-dd')))
-    sorted_movie_data = movie_data.where((movie_data['year'] > 2007) & (movie_data['year'] <= 2017)).drop("genre_ids", "release_date")
+    sorted_movie_data = movie_data.where((movie_data['year'] > 2007) & (movie_data['year'] <= 2017)).drop("release_date")
     #movie_data.write.partitionBy('year').orderBy('popularity').json("./result", mode="overwrite")
     #movie_data.orderBy('year', 'popularity', ascending=False).show(100)
     #sorted_movie_data.show(10)
-    window = Window.partitionBy(sorted_movie_data['year']).orderBy(sorted_movie_data['popularity'].desc())
-    popularity_year_result = sorted_movie_data.select('*', rank().over(window).alias('rank')).filter(col('rank') <= 10).orderBy(col('year'), col('rank'))
+    #use window to sort and select
+    #window = Window.partitionBy(sorted_movie_data['year']).orderBy(sorted_movie_data['popularity'].desc())
+    #popularity_year_result = sorted_movie_data.select('*', rank().over(window).alias('rank')).filter(col('rank') <= 10).orderBy(col('year'), col('rank'))
     popularity_year_result.show(100)
-    popularity_year_result.repartition(1).write.csv('popularity_year_result_1', sep='|', header=True)     
+    popularity_year_result.write.csv('popularity_year_result', sep='|', header=True)     
     #sorted_popularity = movie_data.sort('popularity', ascending=False)
     #sorted_popularity.select(sorted_popularity['title'], sorted_popularity['tagline'], sorted_popularity['popularity']).show(10)
 
