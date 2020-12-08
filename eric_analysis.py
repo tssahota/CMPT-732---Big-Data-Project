bb38af9c8e9ea82e7f7b1077355e5b2837db4e38
hwec0112 <hwec0112@gmail.com>
2020-12-08 05:34:36 -0800

    add plot for task 3

--- eric_analysis.py
+++ eric_analysis.py
@@ -4,67 +4,77 @@ from pyspark.sql import SparkSession, types, Window
 from pyspark.sql.functions import rank, col, to_timestamp, year, month, explode
 
 def main():
-    input_dir = "Processed\ Data"
+    input_dir = "Processed_Data"
     output_dir = "analysis_data"
-    movie_data = spark.read.parquet(input_dir+"/movies_metadata")
+    movie_data = spark.read.parquet(input_dir+"/movies_aggregated_data.parquet")
     #movie_data.describe('popularity').show()
     #movie_data.describe('vote_average').show()
     #remove null data slot
-    #movie_data.show(1)
+    movie_data.show(1)
     #movie_data.printSchema()
     #movie_data = movie_data.where((movie_data['title'].isNotNull()) & (movie_data['release_date'].isNotNull()) & (movie_data['release_date'].isNotNull()) & (movie_data['popularity'].isNotNull()) & (movie_data['genre_ids'].isNotNull()) & (movie_data['profit'].isNotNull()))
-    movie_data = movie_data.na.drop(subset=["title", "popularity", "genre_ids", "budget", "profit"])
+    movie_data = movie_data.na.drop(subset=["title", "popularity", "genre_ids", "profit", 'avg_user_rating'])
     print(movie_data.count())
-    movie_data.show(10)
-
     #create new columns for processing
-    movie_data = movie_data.select((movie_data["profit"]/movie_data["budget"]).alias("return") , movie_data["genre_ids"], movie_data["vote_average"], movie_data["title"], movie_data["popularity"], movie_data["release_date"])
-    print(movie_data.count())
-    movie_data.show(11)
+    movie_data = movie_data.select(movie_data['profit'] , movie_data["genre_ids"], movie_data["vote_average"], movie_data["title"], movie_data["popularity"], movie_data['avg_user_rating'], movie_data["release_date"])
     movie_data = movie_data.withColumn('year', year(to_timestamp(movie_data['release_date'], 'yyyy-MM-dd')))
     movie_data = movie_data.withColumn('month', month(to_timestamp(movie_data['release_date'], 'yyyy-MM-dd')))
-    movie_data = movie_data.where((movie_data['year'] > 2007) & (movie_data['year'] <= 2017)).drop("release_date")
+    movie_data = movie_data.where((movie_data['year'] >= 2000) & (movie_data['year'] <= 2017)).drop("release_date")
     cached_movie_data = movie_data.cache()
+
+    #*** task1
     #use windows to partition and sort data
     popularity_window = Window.partitionBy(cached_movie_data['year']).orderBy(cached_movie_data['popularity'].desc())
     vote_average_window = Window.partitionBy(cached_movie_data['year']).orderBy(cached_movie_data['vote_average'].desc())
-    return_window = Window.partitionBy(cached_movie_data['year']).orderBy(cached_movie_data['return'].desc())
-    # #call window and create new ranking columns
-    # year_return_data = cached_movie_data.drop('month')
-    # year_return_data = year_return_data.select('*', rank().over(popularity_window).alias('poularity_rank'))
-    # year_return_data = year_return_data.select('*', rank().over(vote_average_window).alias('vote_average_rank'))
-    # year_return_data = year_return_data.select('*', rank().over(return_window).alias('return_rank'))
-    # year_return_data = year_return_data.where((col('poularity_rank') <= 10) | (col('vote_average_rank') <= 10) | (col('return_rank') <= 10))
-    # #drop unused columns
-    # year_return_data = year_return_data.drop('poularity_rank', 'vote_average_rank', 'return_rank')
-    # print(year_return_data.count())
-    # year_return_data.show(10)
-    # year_return_data.write.mode('overwrite').parquet(output_dir + "/year_return")
+    profit_window = Window.partitionBy(cached_movie_data['year']).orderBy(cached_movie_data['profit'].desc())
+    avg_user_rating_window = Window.partitionBy(cached_movie_data['year']).orderBy(cached_movie_data['avg_user_rating'].desc())
 
-    #genre analysis
-    genre_data = spark.read.parquet(input_dir+"/genre_details")
-    genre_data.orderBy(genre_data['genre_id'].desc()).show(50)
+    # #call window and create new ranking columns
+    year_profit_data = cached_movie_data.drop('month')
+    year_profit_data = year_profit_data.select('*', rank().over(popularity_window).alias('poularity_rank'))
+    year_profit_data = year_profit_data.select('*', rank().over(vote_average_window).alias('vote_average_rank'))
+    year_profit_data = year_profit_data.select('*', rank().over(profit_window).alias('profit_rank'))
+    year_profit_data = year_profit_data.select('*', rank().over(profit_window).alias('avg_user_rating_rank'))
+    year_profit_data = year_profit_data.where((col('poularity_rank') <= 10) | (col('vote_average_rank') <= 10) | (col('profit_rank') <= 10) | (col('avg_user_rating_rank') <= 10))
+    # #drop unused columns
+    year_profit_data = year_profit_data.drop('poularity_rank', 'vote_average_rank', 'profit_rank', 'avg_user_rating_rank')
+    # year_profit_data.show(10)
+    # year_profit_data.write.mode('overwrite').parquet(output_dir + "/task1")
+
+    #***genre analysis task3
+    genre_data = spark.read.parquet(input_dir+"/genre_details.parquet")
+    #genre_data.orderBy(genre_data['genre_id'].desc()).show(50)
     #drop genres that's not in the list since the amount of data is not enough for analysis
     #genres_list = ['Drama', 'Comedy', 'Thriller', 'Romance', 'Action', 'Horror', 'Crime', 'Adventure', 'Science Fiction', 'Mystery', 'Fantasy', 'Animation']
     #genre_data = genre_data.where(genre_data['genre_name'].isin(genres_list))
-    genre_data.show(12)
     ex_movie_data = cached_movie_data.select('*', explode(cached_movie_data['genre_ids']).alias('genre_id'))
     ex_movie_data = ex_movie_data.drop('genre_ids', 'month')
     genre_movie_data = ex_movie_data.join(genre_data, 'genre_id')
-    genre_movie_data.show(20)
+    #genre_movie_data.show(20)
     #genre_movie_data = genre_movie_data.groupBy('genre_id').count().orderBy('count')
     #genre_movie_data = genre_movie_data.groupBy('genre_id').agg({''})
     genre_pop_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['popularity'].desc())
-    genre_return_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['return'].desc())
+    genre_profit_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['profit'].desc())
     genre_vote_average_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['vote_average'].desc())
+    genre_avg_user_rating_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['avg_user_rating'].desc())
+
     genre_pop_data = genre_movie_data.select('*', rank().over(genre_pop_window).alias('poularity_rank'))
-    genre_pop_data = genre_pop_data.select('*', rank().over(genre_return_window).alias('return_rank'))
+    genre_pop_data = genre_pop_data.select('*', rank().over(genre_profit_window).alias('profit_rank'))
     genre_pop_data = genre_pop_data.select('*', rank().over(genre_vote_average_window).alias('vote_average_rank'))
-    genre_pop_data = genre_pop_data.where((col('poularity_rank') <= 10) | (col('vote_average_rank') <= 10) | (col('return_rank') <= 10))
-    genre_pop_data.show(100)
+    genre_pop_data = genre_pop_data.select('*', rank().over(genre_avg_user_rating_window).alias('avg_user_rating_rank'))
+    genre_pop_data = genre_pop_data.where((col('poularity_rank') <= 10) | (col('vote_average_rank') <= 10) | (col('profit_rank') <= 10) | (col('avg_user_rating_rank') <= 10))
+    #genre_pop_data.show(100)
+    #genre_pop_data.write.mode('overwrite').parquet(output_dir + "/task3")
 
-
-    #genre_movie_data.groupBy('genre_id').agg({'return': 'max', 'popularity': 'max', 'vote_average': 'max'}, title).show(10)
+    #task2
+    #genre_movie_data.show(20)
+    #genre_movie_data.groupBy('genre_id', 'year').max('return', 'vote_average', 'popularity').show()
+    # window = Window.partitionBy(genre_movie_data['year']).groupBy('genre_id').agg({'popularity': 'max'}).orderBy(genre_movie_data['popularity'].desc())
+    # test = genre_movie_data.select('*', rank().over(genre_pop_window).alias('poularity_rank'))
+    # test.show(10)
+    # genre_pop_window = Window.partitionBy(genre_movie_data['genre_id'], genre_movie_data['year']).orderBy(genre_movie_data['popularity'].desc())
+    # genre_return_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['return'].desc())
+    # genre_vote_average_window = Window.partitionBy(genre_movie_data['genre_id']).orderBy(genre_movie_data['vote_average'].desc())
 
 if __name__ == '__main__':
     spark = SparkSession.builder.appName("temporal_trend_analysis").getOrCreate()
--- eric_ui.py
+++ eric_ui.py
@@ -12,76 +12,128 @@ app = dash.Dash(__name__)
 
 # ------------------------------------------------------------------------------
 # Import and clean data (importing csv into pandas)
-path = './analysis_data/year_return' # use your path
-filenames = glob.glob(path+"/*.parquet")
-dfs = []
-for filename in filenames:
-    #dfs.append(pd.read_csv(filename, sep='|', header=0))
-    dfs.append(pd.read_parquet(filename))
-# Concatenate all data into one DataFrame
-df = pd.concat(dfs, ignore_index=True)
-#print(df)
+path_list = ['./analysis_data/task1', './analysis_data/task3'] # use your path
+task_list = ['task1', 'task3']
+df = {}
+for i, path in enumerate(path_list):
+    filenames = glob.glob(path+"/*.parquet")
+    dfs = []
+    for filename in filenames:
+        dfs.append(pd.read_parquet(filename))
+    # Concatenate all data into one DataFrame
+    df[task_list[i]] = pd.concat(dfs, ignore_index=True)
+
+
+genre_list = []
+for genre_name in df['task3']['genre_name'].unique():
+    genre_list.append({"label": genre_name, "value": genre_name})
+
+year_list = []
+for i in range(18):
+    year_list.append({"label": 2000+(17-i), "value": 2000+(17-i)})
+
+col_label = {}
+col_label['vote_average'] = 'Vote Average'
+col_label['avg_user_rating'] = 'Average User Rating (Max=5)'
+col_label['popularity'] = 'Popularity'
+col_label['profit'] = 'Profit'
+
+col_list = [{"label": col_label["popularity"], "value": "popularity"},
+            {"label": col_label["profit"], "value": "profit"},
+            {"label": col_label['vote_average'], "value": "vote_average"},
+            {"label": col_label['avg_user_rating'], "value": "avg_user_rating"}]
+
 # ------------------------------------------------------------------------------
 # App layout
 app.layout = html.Div([
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
-    dcc.Dropdown(id="slct_year",
-                 options=[
-                     {"label": "2017", "value": 2017},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2015", "value": 2015},
-                     {"label": "2014", "value": 2014},
-                     {"label": "2013", "value": 2013},
-                     {"label": "2012", "value": 2012},
-                     {"label": "2011", "value": 2011},
-                     {"label": "2010", "value": 2010},
-                     {"label": "2009", "value": 2009},
-                     {"label": "2008", "value": 2008}],
-                 multi=False,
-                 value=2017,
-                 clearable=False,
-                 style={'width': "50%"}
-                 ),
-    dcc.Dropdown(id="slct_col",
-                 options=[
-                     {"label": "Popularity", "value": "popularity"},
-                     {"label": "Return", "value": "return"},
-                     {"label": "Vote Average", "value": "vote_average"}],
-                 multi=False,
-                 value="popularity",
-                 clearable=False,
-                 style={'width': "50%"}
-                 ),
-    html.Div(id='output_container'),
-    html.Br(),
-
-    dcc.Graph(id='bar_chart')
-
+    html.Div(id='task1_container', children=[
+        html.H1(id='header_task1', style={'text-align': 'center'}),
+        dcc.Dropdown(id="slct_year",
+                    options=year_list,
+                    multi=False,
+                    value=2017,
+                    clearable=False,
+                    style={'width': "50%"}
+                    ),
+        dcc.Dropdown(id="slct_col",
+                    options=col_list,
+                    multi=False,
+                    value="popularity",
+                    clearable=False,
+                    style={'width': "50%"}
+                    ),
+        html.Br(),
+        dcc.Graph(id='task1_bar_chart')
+    ]),
+    
+    html.Div(id='task3_container', children=[
+        html.H1(id='header_task3', style={'text-align': 'center'}),
+                dcc.Dropdown(id="slct_genre_task3",
+                    options=genre_list,
+                    multi=False,
+                    value='Adventure',
+                    clearable=False,
+                    ),
+        dcc.Dropdown(id="slct_col_task3",
+                    options=col_list,
+                    multi=False,
+                    value="popularity",
+                    clearable=False,
+        ),
+        html.Br(),
+        dcc.Graph(id='task3_bar_chart')
+    ]),
 ])
 
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
+#***Task1 callback
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='bar_chart', component_property='figure')],
+    [Output(component_id='header_task1', component_property='children'),
+     Output(component_id='task1_bar_chart', component_property='figure')],
     [Input(component_id='slct_year', component_property='value'),
     Input(component_id='slct_col', component_property='value')]
 )
 def update_graph(slct_year, slct_col):
-    print(slct_year, slct_col)
-
-    container = f"Showing the movie ranking by {slct_col} in {slct_year}"
-
-    dff = df.copy()
+    print('task1 update', slct_year, slct_col)
+    container = f"10 Most {col_label[slct_col]} Movies in {slct_year}"
+    dff = df["task1"].copy()
     #filter col
     dff = dff[dff["year"] == slct_year].sort_values(by=slct_col, ascending=False).head(10).sort_values(by=slct_col, ascending=True)
-    print("dff", dff)
+    #print("task 1 dff", dff)
     #filter rows
     #dff = dff[dff["Affected by"] == "Varroa_mites"]
-    fig = px.bar(data_frame=dff, y='title', x=slct_col, orientation='h', text=slct_col, template="plotly_dark" )
+    #text=slct_col,
+    fig = px.bar(data_frame=dff, y='title', x=slct_col, orientation='h', text=slct_col, template="ggplot2", colors=[
+            "#1b9e77",
+            "#d95f02",
+            "#7570b3",
+            "#e7298a",
+            "#66a61e",
+            "#e6ab02",
+            "#a6761d",
+            "#666666",
+            "#1b9e77",
+        ] )
     return container, fig
 
+#***Task3 callback
+@app.callback(
+    [Output(component_id='header_task3', component_property='children'),
+    Output(component_id='task3_bar_chart', component_property='figure')],
+    [Input(component_id='slct_genre_task3', component_property='value'),
+    Input(component_id='slct_col_task3', component_property='value')]
+)
+def update_graph(slct_genre, slct_col):
+    print('task3 update', slct_genre, slct_col)
+    container = f"10 Most {col_label[slct_col]} Movies in {slct_genre} (2000-2017)"
+    dff = df["task3"].copy()
+    #filter col
+    dff = dff[dff["genre_name"] == slct_genre].sort_values(by=slct_col, ascending=False).head(10).sort_values(by=slct_col, ascending=True)
+    print("task3_dff", dff)
+    fig = px.bar(data_frame=dff, y='title', x=slct_col, orientation='h', text=slct_col, template="ggplot2", color=slct_col)
+    return container, fig
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
